64956dc1cfe4265609019e061b51e9cc04439d4b
shana parveen m.a <shanaanwar26@gmail.com>
2022-09-02 10:54:04 +0530

    initial commit

---
+++ resources/views/reg.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 align="center">Customer Registration</h1>
+    <table border="0"cellspacing="20px">
+        <tr>
+            <form action="" method="get">
+            <td>Customer Name:</td>
+            <td><input type="text" name="" id=""></td>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>Gender:</td>
+                <td><input type="radio" name="gender" id="">Male
+                <input type="radio" name="gender" id="">Female</td>
+            </tr>
+            <tr>
+                <td>Address:</td>
+                <td><textarea name="" id="" cols="40" rows="10"></textarea></td>
+            </tr>
+            <tr>
+                <td>Telephone No:</td>
+                <td><input type="number" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>Hobbies:</td>
+                <td> <select name="" id="">
+                    <option value=""selected>Reading</option>
+                    <option value="">travelling</option>
+                    <option value="">writing</option>
+                    <option value="">learning</option>
+                </select> <br><br></td>
+
+            </tr>
+            <tr>
+                <td><button>SUBMIT</button></td>
+            </tr>
+            </form>
+        </tr>
+    </table>
+    
+</body>
+</html>
